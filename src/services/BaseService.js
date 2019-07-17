79c5e20b715b576107aac6e096696f59626e73bf
Fran Enriquez <fjenriglez@gmail.com>
2019-07-17 20:10:15 +0200

    fix: request to api

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const REACT_APP_API_URL = 'http://localhost:3001';
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 
 const http = axios.create({
